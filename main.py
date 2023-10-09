425f4d1618bc7f1bcd02fb46f1ebe809e9e96513
Aden <aing810@aucklanduni.ac.nz>
2023-10-10 00:12:21 +1300

    fixed limits

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import tkinter as tk
 from tkinter import ttk
 
-def on_slider_change(value):
+def on_slider_change(metric_changed):
+    # If the total exceeds 1, reduce the most recently adjusted slider
+    total = sum(slider.get() for slider in sliders.values())
+    if total > 1:
+        excess_value = total - 1
+        new_value = sliders[metric_changed].get() - excess_value
+        sliders[metric_changed].set(new_value)
+
     # Update the displayed values for the sliders
     for metric, slider in sliders.items():
         slider_labels[metric].config(text=f"{metric}: {slider.get():.2f}")
-    update_total_label()
 
-def update_total_label():
     # Update the total label
     total = sum(slider.get() for slider in sliders.values())
     total_label.config(text=f"Total: {total:.2f}")
     
-    # Check if the total is approximately 1
-    if 0.99 <= total <= 1.01:
-        total_label.config(fg='green')
+    # Check if the rounded total is exactly 1
+    if round(total, 2) == 1:
         calc_button.config(state=tk.NORMAL)
     else:
-        total_label.config(fg='red')
         calc_button.config(state=tk.DISABLED)
 
 def calculate_ranking():
@@ -35,13 +38,17 @@ def reset_to_defaults():
     for metric, weight in default_weights.items():
         sliders[metric].set(weight)
         slider_labels[metric].config(text=f"{metric}: {weight:.2f}")
-    update_total_label()
+    on_slider_change(None)
 
 root = tk.Tk()
 root.title("Rank Towns")
 
 frame = ttk.LabelFrame(root, text="Adjust Weights", padding=(10, 5))
 frame.pack(pady=20, padx=20, fill=tk.BOTH, expand=True)
+
+# Define total label before creating sliders
+total_label = ttk.Label(frame, text="Total: 1.00", font=('Arial', 10))
+total_label.pack(pady=10)
 
 metrics = [
     'Wind speed',
@@ -59,7 +66,8 @@ for metric in metrics:
     label = ttk.Label(row_frame, text=metric, width=20)
     label.grid(row=0, column=0)
     
-    slider = ttk.Scale(row_frame, from_=0, to_=1, orient=tk.HORIZONTAL, length=200, command=on_slider_change)
+    slider = ttk.Scale(row_frame, from_=0, to_=1, orient=tk.HORIZONTAL, length=200, 
+                       command=lambda v, metric=metric: on_slider_change(metric))
     default_weights = {
         'Wind speed': 0.3125,
         'Atmospheric pressure': 0.21875,
@@ -76,10 +84,7 @@ for metric in metrics:
     sliders[metric] = slider
     slider_labels[metric] = value_label
 
-total_label = ttk.Label(frame, text="Total: 1.00", font=('Arial', 10, 'bold'))
-total_label.pack(pady=10)
-
-calc_button = ttk.Button(root, text="Calculate Ranking", command=calculate_ranking, state=tk.NORMAL)
+calc_button = ttk.Button(root, text="Calculate Ranking", command=calculate_ranking, state=tk.DISABLED)
 calc_button.pack(pady=10)
 
 reset_button = ttk.Button(root, text="Reset to Defaults", command=reset_to_defaults)
